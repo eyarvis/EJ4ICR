3a0269e7afe3678df94991f7b424ea066c46f9be
emily.yarvis <emilyyarvis@Ubuntu.myguest.virtualbox.org>
2023-07-12 16:15:21 -0700

    Heatmap Plotting

--- EJRP.py
+++ EJRP.py
@@ -4,9 +4,19 @@ import matplotlib.pyplot as plt
 from itertools import combinations
 from scipy.stats import pearsonr
 import sys
-import seaborn as sns
+import seaborn as sb
 
-cities = ['Houston', 'Detroit', 'Portland']
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows',None)
+#pd.set_option('display.max_rowwidth',None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', None)
+
+eLabel = ['PM 2.5','Ozone(ppb)','Diesel PM (ug/m3)','Cancer Risk','Resp. Hazard Index','Toxic Releases To Air','Traffic(prox. & vol.)',
+          'Lead Paint Indicator','Superfund Prox.','RMP Prox.','Hazardous Waste Prox.','Underground Storage Tank Indicator','Waste Water Discharge Indicator']
+dLabel =['Demo. Index','Supp. Demo. Index ','POC Pop.','Low Income Pop.','Unemployed','Low English Household','Pop. w/ < HS Ed.']
+
+cities = ['Portland', 'Houston', 'Detroit']
 datasets = ['1', '2', '3', '4', '5']#in string form
 newDataSets = [ 1, 2, 3, 4, 5]#in num form
 
@@ -37,26 +47,6 @@ for city in cities:#this defines valueData and makes it useable as an array of t
 
 
 
-'''''#old way of doing something
-houston1 = data['Houston'][0]
-houston2 = data['Houston'][1]
-houston3 = data['Houston'][2]
-houston4 = data['Houston'][3]
-houston5 = data['Houston'][4]
-
-portland1 = data['Portland'][0]
-portland2 = data['Portland'][1]
-portland3 = data['Portland'][2]
-portland4 = data['Portland'][3]
-portland5 = data['Portland'][4]
-
-detroit1 = data['Detroit'][0]
-detroit2 = data['Detroit'][1]
-detroit3 = data['Detroit'][2]
-detroit4 = data['Detroit'][3]
-detroit5 = data['Detroit'][4]
-'''
-
 
 
 test = data['Portland'][4]
@@ -82,26 +72,6 @@ d2Tract= "26163512800"
 d3Tract= "26163531800"
 d4Tract= "26163501500"
 d5Tract= "26163535000"
-'''
-
-'''''#old p1value declaration
-p1value = (data['Portland'][0])["Value"]
-p2value = portland2["Value"]
-p3value = portland3["Value"]
-p4value = portland4["Value"]
-p5value = portland5["Value"]
-
-h1value = houston1["Value"]
-h2value = houston2["Value"]
-h3value = houston3["Value"]
-h4value = houston4["Value"]
-h5value = houston5["Value"]
-
-d1value = detroit1["Value"]
-d2value = detroit2["Value"]
-d3value = detroit3["Value"]
-d4value = detroit4["Value"]
-d5value = detroit5["Value"]
 '''
 
 '''#old table
@@ -160,90 +130,94 @@ print(col)
 '''
 
 
-'''old arrays for all the cities
-p1arr =np.array(p1value)
-p2arr =np.array(p2value)
-p3arr =np.array(p3value)
-p4arr =np.array(p4value)
-p5arr =np.array(p5value)
-
-h1arr=np.array(h1value)
-h2arr=np.array(h2value)
-h3arr=np.array(h3value)
-h4arr=np.array(h4value)
-h5arr=np.array(h5value)
-
-d1arr=np.array(d1value)
-d2arr=np.array(d2value)
-d3arr=np.array(d3value)
-d4arr=np.array(d4value)
-d5arr=np.array(d5value)
-'''
-
 
 
 #creates array that holds all the different tracts and factors in one table
 ###tractArr=np.array([p1value,p2value,p3value,p4value,p5value,h1value,h2value,h3value,h4value,h5value,d1value,d2value,d3value,d4value,d5value])
 arr=[]
+
+factorDataPerCity = {}
 for city in cities:
+    specificCityFactorData = []
     for i in newDataSets:
         arr.append(valueData[city][i-1])
+        specificCityFactorData.append(valueData[city][i-1])
+    factorDataPerCity[city]=specificCityFactorData
 
 tractArr=np.array(arr)
-print("HELOOLOLO")
-print(tractArr)
+
 
 #creates pretty data frame for that table
+
+cityDataFrames = {}
+#creates data frame for all tracts factors
 tractdf = pd.DataFrame(tractArr, columns = ['Factor 1','Factor 2','Factor 3','Factor 4','Factor 5',
                                             'Factor 6','Factor 7','Factor 8','Factor 9','Factor 10',
                                             'Factor 11','Factor 12','Factor 13','Factor 14','Factor 15',
                                             'Factor 16','Factor 17','Factor 18','Factor 19','Factor 20',])
 
+
+for city in cities:#creates data frame for one cities tract factors
+    df =pd.DataFrame(factorDataPerCity[city], columns = ['Factor 1','Factor 2','Factor 3','Factor 4','Factor 5',
+                                            'Factor 6','Factor 7','Factor 8','Factor 9','Factor 10',
+                                            'Factor 11','Factor 12','Factor 13','Factor 14','Factor 15',
+                                            'Factor 16','Factor 17','Factor 18','Factor 19','Factor 20',])
+    cityDataFrames[city]=df
+    
+    
+
+
 dFactorDict = ['Factor 14','Factor 15','Factor 16','Factor 17','Factor 18','Factor 19','Factor 20']
+eFactorDict = ['Factor 1','Factor 2','Factor 3','Factor 4','Factor 5','Factor 6','Factor 7','Factor 8','Factor 9','Factor 10','Factor 11','Factor 12','Factor 13']
+
+
 for d in dFactorDict:#gets rid of the percentages and changes percent in dfactors to decimals
     tractdf[d]=tractdf[d].str.rstrip("%").astype(float)/100
+    for city in cities:
+        df = cityDataFrames[city]
+        df[d]=df[d].str.rstrip("%").astype(float)/100
+        #houstondf[d]=houstondf[d].str.rstrip("%").astype(float)/100
+        #detroitdf[d]=detroitdf[d].str.rstrip("%").astype(float)/100
 
-
-print("TRACTDF")
-print(tractdf)
-
-
+print("PORTLAND")
+print(cityDataFrames['Portland'])
 #Enviormental Factors
-factor1 = np.array(tractdf['Factor 1'],dtype=float)
-factor2 = np.array(tractdf['Factor 2'],dtype=float)
-factor3 = np.array(tractdf['Factor 3'],dtype=float)
-factor4 = np.array(tractdf['Factor 4'],dtype=float)
-factor5 = np.array(tractdf['Factor 5'],dtype=float)
-factor6 = np.array(tractdf['Factor 6'],dtype=float)
-factor7 = np.array(tractdf['Factor 7'],dtype=float)
-factor8 = np.array(tractdf['Factor 8'],dtype=float)
-factor9 = np.array(tractdf['Factor 9'],dtype=float)
-factor10 = np.array(tractdf['Factor 10'],dtype=float)
-factor11 = np.array(tractdf['Factor 11'],dtype=float)
-factor12 = np.array(tractdf['Factor 12'],dtype=float)
-factor13 = np.array(tractdf['Factor 13'],dtype=float)
+factorArrDict = {}
 
-#these ones are the percent and thus the Demographic Factors
-factor14 = np.array(tractdf['Factor 14'],dtype=float)
-factor15 = np.array(tractdf['Factor 15'],dtype=float)
-factor16 = np.array(tractdf['Factor 16'],dtype=float)
-factor17 = np.array(tractdf['Factor 17'],dtype=float)
-factor18 = np.array(tractdf['Factor 18'],dtype=float)
-factor19 = np.array(tractdf['Factor 19'],dtype=float)
-factor20 = np.array(tractdf['Factor 20'],dtype=float)
+for e in eFactorDict:
+    factorArrDict[e] = np.array(tractdf[e],dtype=float)
 
-factorArr = np.array([factor1,factor2,factor3,factor4,factor5,factor6,factor7,factor8,factor9,factor10,factor11,factor12,factor13,factor14,factor15,factor16,factor17,factor18,factor19,factor20])
+for d in dFactorDict:
+    factorArrDict[d] = np.array(tractdf[d],dtype=float)
+
+
+#factorArr = np.array([factor1,factor2,factor3,factor4,factor5,factor6,factor7,factor8,factor9,factor10,factor11,factor12,factor13,factor14,factor15,factor16,factor17,factor18,factor19,factor20])
+#testFactorArr =[factor1,factor2,factor3,factor4,factor5,factor6,factor7,factor8,factor9,factor10,factor11,factor12,factor13,factor14,factor15,factor16,factor17,factor18,factor19,factor20] 
+
+
 #stores only the enviormental factor data
-enviormentalFactorArr = np.array([factor1,factor2,factor3,factor4,factor5,factor6,factor7,factor8,factor9,factor10,factor11,factor12,factor13,])
-#stores only the demographic data
-demographicFactorArr = np.array([factor14,factor15,factor16,factor17,factor18,factor19,factor20])
+eConvertArr = []
+for e in eFactorDict:
+   eConvertArr.append(factorArrDict[e])
+enviormentalFactorArr = np.array(eConvertArr)
 
-'''#playing with plotting
-x=factor1
-y=factor16
-plt.plot(x,y,'ro')
+#stores only demographic factor data
+dConvertArr = []
+for d in dFactorDict:
+    dConvertArr.append(factorArrDict[d])
+demographicFactorArr = np.array(dConvertArr)
+
+
+#factordf = pd.DataFrame(columns=(['e#','d#','r']))
+
+corrFactorAll = tractdf.corr()#also a perfectly good representation of the correlation coefficient between ALL factors
+#print("CORRRRERER")
+#print(corrFactor)
+#plot all individual cities
+
+#dataplot = sb.heatmap(corrFactorAll,annot=True,linewidths=0.5, linecolor='white')
+
 #plt.show()
-'''#################################
 
 
 
@@ -272,10 +246,7 @@ for i in range(0,91):
     #print(i,': ',x,',',y,':',p)
     #print('Pearsons correlation: %.3f' % p)
 
-#print(pearsonCorrelationResults)
-#print(enviormentalFactorArr)
-#need to make a table of the data and the corresponding factors to coefficients and stuff
-#df['column_name']=pd.Series(arr)
+
 
 #creates the df to display all the r values 
 correlationdf = pd.DataFrame(columns=(['e#','d#','r']))
@@ -283,19 +254,18 @@ correlationdf['e#']=pd.Series(efactor)
 correlationdf['d#']=pd.Series(dfactor)
 correlationdf['r']=pd.Series(pearsonCorrelationResults)
 
-#print(correlationdf)
-#print(variablesCol)
+
 variables = np.array(variablesCol)
 efactorName= []
 dfactorName= []
 
 for i in range(0,13):
     efactorName.append(variables[i])
-    print(i)
+    
 
 for i in range(0,7):
     dfactorName.append(variables[13+i])
-    print(i)
+    
 
 
 efactorNameFinal = []
@@ -306,11 +276,6 @@ for i in range(0,91):
     dfactorNameFinal.append(dfactorName[dfactor[i]])
 
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows',None)
-#pd.set_option('display.max_rowwidth',None)
-pd.set_option('display.width', None)
-pd.set_option('display.max_colwidth', None)
 
 #creating new df to display correlation stuff
 correlationdf = pd.DataFrame(columns=(['E#','Enviormental Factor','D#','Demographic Factor','r']))
@@ -321,4 +286,57 @@ correlationdf['Demographic Factor']=pd.Series(dfactorNameFinal)
 correlationdf['r']=pd.Series(pearsonCorrelationResults)
 print(correlationdf)
 
+
+
 #Creating new df to display correlation stuff in matrix form
+'''''Seaborn Heat Map That Doesnt Really Work
+efactorData = []
+totalCount=0
+for i in range(0,13):
+    d = []
+    for j in range(0,7):
+        d.append(pearsonCorrelationResults[totalCount])
+        totalCount = totalCount+1
+    efactorData.append(d)
+        
+    
+
+
+matrixCorr = pd.DataFrame()
+matrixCorr['Demographic Factors'] = pd.Series(dLabel)
+for i in range(0,13):
+    matrixCorr[eLabel[i]] = pd.Series(efactorData[i])
+
+
+print(matrixCorr)
+'''
+################################################################################################
+#Creating heatmaps of all diff cities
+
+''''Heres how the below works
+##############################################################
+#Heres how this Works:
+#fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12,12))
+#fig, (first plot,second plot) = plt.suibplots(# of plots, # of columns,figsize=(what ,by what))
+###################################################
+'''
+a= []
+fig, ax = plt.subplots(1, 3, figsize=(24,24))
+graphs=0
+plt.yticks(rotation=45)
+#plt.margins(x=1)
+for city in cities:
+    plot = cityDataFrames[city]
+    g = sb.heatmap(plot.corr(),ax=ax[graphs],linewidths=0.25, linecolor='white',square = True)
+    g.set(xlabel = city)
+    graphs = graphs+1
+''''
+portland = (cityDataFrames['Portland'])
+Houston = (cityDataFrames['Houston'])
+Detroit = (cityDataFrames['Detroit'])
+sb.heatmap(portland.corr(),ax=ax[0])
+sb.heatmap(Houston.corr(),ax=ax[1])
+sb.heatmap(Detroit.corr(),ax=ax[2])
+'''
+plt.margins(x=0)
+plt.show()
