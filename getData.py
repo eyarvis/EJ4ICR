74cdaf663f17c870248f29a3f2ffbf88eeead774
emily.yarvis <emilyyarvis@Ubuntu.myguest.virtualbox.org>
2023-07-17 07:56:44 -0700

    Fixed Heatmaps

--- getData.py
+++ getData.py
@@ -3,8 +3,8 @@ import seaborn as sb
 import matplotlib.pyplot as plt
 from pylab import savefig
 
-#pd.set_option('display.max_columns', None)
-#pd.set_option('display.max_rows',None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows',None)
 #pd.set_option('display.max_rowwidth',None)
 #pd.set_option('display.width', None)
 #pd.set_option('display.max_colwidth', None)
@@ -17,62 +17,78 @@ def corrPlot(corrdf):#function to plot correlation heatmaps
     sb.heatmap(plot)
     plt.show()
 
-def plotForState(stateName,stateIndex):#this will print a heatmap for a given state
+def plotForState(stateName,stateIndex,colArr):#this will print a heatmap for a given state
     totalDataPoints = len(stateIndex[stateName])
+    dataStart = stateIndex[stateName][0]
+    print("DATA START",dataStart)
+   
+    dataEnd = stateIndex[stateName][totalDataPoints-1]
+    print("DATA END", dataEnd)
     newColArr = []
 
-    for j in range(0,19):
+    Tdf = pd.DataFrame(columns = (['PM 2.5','Ozone(ppb)','Diesel PM (ug/m3)','Cancer Risk','Resp. Hazard Index','Toxic Releases To Air','Traffic(prox. & vol.)',
+          'Lead Paint Indicator','Superfund Prox.','RMP Prox.','Hazardous Waste Prox.','Underground Storage Tank Indicator','Waste Water Discharge Indicator','PM25-F1','OZONE-F2','DSLPM-F3','CANCER-F4','RESP-F5','RSEI_AIR-F6','PTRAF-F7','PRE1960PCT-F8','PNPL-F9','PRMP-F10','PTSDF-F11','UST-F12','PWDIS-F13',
+             'DEMOGIDX_2-F14','DEMOGIDX_5-F15','PEOPCOLORPCT-F16','LOWINCPCT-F17','UNEMPPCT-F18','LINGISOPCT-F19','LESSHSPCT-F20']))
+
+
+
+    for j in range(0,20):
         newArr = []
         currentFactor = colArr[j]
-        for i in range(0,totalDataPoints):
+        for i in range(dataStart,(dataEnd+1)):
             newArr.append(currentFactor[i])
         newColArr.append(newArr)
 
     testdf = pd.DataFrame(newColArr)
-    print(testdf)
 
-
-    testdf = testdf.transpose()
-    print(testdf)
-    testdf = testdf.corr()
-    print(testdf)
-
-    g =sb.heatmap(testdf)
+    Tdf = testdf.transpose()
+    
+    
+    print("TESTDF TO COMPARE")
+    print(Tdf[0])
+    finaldf = Tdf.corr()
+    
+    plt.figure(figsize=(20,10))
+    g =sb.heatmap(finaldf,annot=True,linewidths=0.25, linecolor='white')
     g.set(xlabel = stateName)
+   
     plt.show()
 
 
 
-df = pd.read_csv('RPCSV/CSVTables/totalTracts.csv')#this is the file that holds all of the tracts
+df = pd.read_csv('RPCSV/CSVTables/totalTracts2.csv')#this is the file that holds all of the tracts
 
 
 #these arrays hold the lists of factors
 eLabel = ['PM 2.5','Ozone(ppb)','Diesel PM (ug/m3)','Cancer Risk','Resp. Hazard Index','Toxic Releases To Air','Traffic(prox. & vol.)',
           'Lead Paint Indicator','Superfund Prox.','RMP Prox.','Hazardous Waste Prox.','Underground Storage Tank Indicator','Waste Water Discharge Indicator']
 dLabel =['Demo. Index','Supp. Demo. Index ','POC Pop.','Low Income Pop.','Unemployed','Low English Household','Pop. w/ < HS Ed.']
-tableLabels=['PM25-F1','OZONE-F2','DSLPM-F3','CANCER-F4','RESP-F5','RSEI_AIR-F6','PTRAF-F7','PRE1960PCT-F8','PNPL-F9','PRMP-F10','UST-F12','PWDIS-F13',
+tableLabels=['PM25-F1','OZONE-F2','DSLPM-F3','CANCER-F4','RESP-F5','RSEI_AIR-F6','PTRAF-F7','PRE1960PCT-F8','PNPL-F9','PRMP-F10','PTSDF-F11','UST-F12','PWDIS-F13',
              'DEMOGIDX_2-F14','DEMOGIDX_5-F15','PEOPCOLORPCT-F16','LOWINCPCT-F17','UNEMPPCT-F18','LINGISOPCT-F19','LESSHSPCT-F20']
 
-states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 
+states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 
           'California', 'Colorado', 'Connecticut', 
           'Delaware', 'District of Columbia', 'Florida', 'Georgia', 
-          'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 
+           'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 
           'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 
-          'Minnesota', 'Minor Outlying Islands', 'Mississippi', 'Missouri', 
+          'Minnesota',  'Mississippi', 'Missouri', 
           'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 
           'New Mexico', 'New York', 'North Carolina', 'North Dakota', 
-          'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 
-          'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 
-          'Tennessee', 'Texas', 'Virgin Islands', 'Utah', 'Vermont', 'Virginia', 
+          'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 
+          'Rhode Island', 'South Carolina', 'South Dakota', 
+          'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 
           'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 stateTract={}#hold state name and all corresponding tracts
 stateIndex={}#holds state names and all correspondindg row index of data
 
-colArr = []
+colArr = []#array of all the factors values from the tab;le
+index = 0
 for label in tableLabels:#creates an array of all the factors and excludes the state and tract stuff
-    print(label)
+    #print(index)
+    #index = index+1
+    #print(label)
     ls = df[label]
-    print(ls)
+    #print(ls)
     colArr.append(ls)
 
 #Prints heatmap of every single different tract
@@ -105,12 +121,13 @@ for i in range(0,totalTractCount):
 
 
 #making a heatmap for alabama to test before writting function
-plotForState('Alabama',stateIndex)
+#print(stateIndex['Alaska'])
+#plotForState('Alaska',stateIndex,colArr)
 
 ''''#this will print all the heatmaps for all the states
 for state in states:
     plotForState(state,stateIndex)
-''''
+'''
 
 ''''#this is old test code
 totalAlabama= len(stateIndex['Alabama'])
@@ -137,3 +154,7 @@ sb.heatmap(testdf)
 plt.show()
 '''
 ##########################################################################
+
+for state in states:
+    print(state)
+    plotForState(state,stateIndex,colArr)
